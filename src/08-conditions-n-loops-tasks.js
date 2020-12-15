35a16f74d4f4c583589b26b0f4fb8ceb5d8ebf4a
bobrovsi1988 <bobrovsi1988@gmail.com>
2020-12-15 11:31:15 +0300

    bla

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -127,8 +127,18 @@ function isTriangle(a, b, c) {
  *   { top:20, left:20, width: 20, height: 20 }    =>  false
  *
  */
-function doRectanglesOverlap(/* rect1, rect2 */) {
-  throw new Error('Not implemented');
+function doRectanglesOverlap(rect1, rect2) {
+  if (rect1.left >= rect2.left + rect2.width || rect2.left >= rect1.left + rect1.width) {
+    return false;
+  }
+
+
+  // If one rectangle is above other
+  if (rect1.top <= rect2.top - rect2.height || rect2.top <= rect1.top - rect1.height) {
+    return false;
+  }
+
+  return true;
 }
 
 
